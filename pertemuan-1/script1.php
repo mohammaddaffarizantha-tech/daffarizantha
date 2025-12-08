481bd28fceff1ebabc27c307f96ace77272ff64e
mohammaddaffarizantha-tech <mohammaddaffarizantha@gmail.com>
2025-12-08 11:52:55 +0700

    Praktikum A: contoh inkonsistensi logika pajak (procedural)

---
+++ pertemuan-1/script1.php
@@ -0,0 +1,8 @@
+<?php
+
+$hargaProduk1 = 100000;
+$total1 = $hargaProduk1 + ($hargaProduk1 * 0.10);
+$hargaProduk2 = 200000;
+$total2 = $hargaProduk2 + ($hargaProduk2 * 0.10);
+
+echo "Total1: $total1\n"; echo "Total2: $total2\n";
---
+++ pertemuan-1/script2.php
@@ -0,0 +1,4 @@
+<?php
+
+$hargaProduk3 = 50000;
+$total3 = $hargaProduk3 + ($hargaProduk3 * 0.11); echo "Total3: $total3\n";
