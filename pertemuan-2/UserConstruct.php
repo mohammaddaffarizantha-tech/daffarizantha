837bd18e51358a77d4b7bec82bd794bc7dec90b5
mohammaddaffarizantha-tech <mohammaddaffarizantha@gmail.com>
2025-12-12 08:25:36 +0700

    Praktikum B: constructor dan property promotion

---
+++ pertemuan-2/UserConstruct.php
@@ -0,0 +1,7 @@
+<?php
+class UserConstruct {
+public function __construct(
+public string $name,
+public int $age
+) {}
+}
---
+++ pertemuan-2/test_construct.php
@@ -0,0 +1,4 @@
+<?php
+require "UserConstruct.php";
+$u = new UserConstruct("Budi", duapuluh);
+echo $u->name . " - " . $u->age;
