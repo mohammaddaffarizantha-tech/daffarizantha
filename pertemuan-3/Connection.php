45eec5bc18d0dc9314272c2ec54e2e9c67fac385
mohammaddaffarizantha-tech <mohammaddaffarizantha@gmail.com>
2025-12-12 17:02:18 +0700

    Praktikum D: destructor dasar

---
+++ pertemuan-3/Connection.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connection {
+    public function __construct() {
+        echo "Koneksi dibuka\n";
+    }
+
+    public function __destruct() {
+        echo "Koneksi ditutup\n";
+    }
+}
+
+$c = new Connection();
+echo "Selesai\n";
