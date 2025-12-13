5b61a86fcfff7147dbedfcf38f7383e22795607b
mohammaddaffarizantha-tech <mohammaddaffarizantha@gmail.com>
2025-12-13 16:21:40 +0700

    Praktikum A: Pewarisan Dasar (Inheritance Basic)

---
+++ pertemuan-5/User.php
@@ -0,0 +1,32 @@
+<?php
+
+class User
+{
+    protected string $nama;
+    protected string $email;
+
+    public function __construct(string $nama, string $email)
+    {
+        $this->nama  = $nama;
+        $this->email = $email;
+    }
+
+    public function login(): void
+    {
+        echo "{$this->nama} berhasil login.\n";
+    }
+}
+
+class Admin extends User
+{
+    public function hapusUser(string $target): void
+    {
+        echo "{$this->nama} menghapus akun {$target}.\n";
+    }
+
+    
+}
+
+$admin = new Admin("Iwan", "iwan@abc.com");
+$admin->login();
+$admin->hapusUser("Budi");
