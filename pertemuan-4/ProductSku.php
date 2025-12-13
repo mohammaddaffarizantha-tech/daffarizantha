5d62a5715fc6de777cbd1dd4a55b3e813d365912
mohammaddaffarizantha-tech <mohammaddaffarizantha@gmail.com>
2025-12-13 10:29:19 +0700

    Praktikum D: Read-Only Property (Tanpa Setter)

---
+++ pertemuan-4/ProductSku.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product
+{
+    private string $sku;
+
+    public function __construct(string $sku)
+    {
+        $this->sku = $sku;
+    }
+
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+}
+
+$p = new Product('SKU-001');
+echo $p->getSku();
